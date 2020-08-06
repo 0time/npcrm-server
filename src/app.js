5f822f783b528163ac18f87fb7de11199cc425f8
Henry Brown <h@0ti.me>
2020-08-05 20:16:39 -0500

    Add merge for context

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const CONTEXT_DEFAULTS = require('./defaults/context');
 const config = require('config');
+const merge = require('lodash.merge');
 const routes = require('./routes');
 
 module.exports = appContext => {
-  const context = Object.assign(
-    {},
-    CONTEXT_DEFAULTS,
-    { config, routes },
-    appContext,
-  );
+  const context = merge({}, CONTEXT_DEFAULTS, { config, routes }, appContext);
 
   return context.server.start(context);
 };

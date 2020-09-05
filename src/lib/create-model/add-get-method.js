87e330179843100047773d567b593b8250115440
Henry Brown <h@0ti.me>
2020-09-05 14:37:38 -0500

    Promisify the get method so it does not crash the request, add tests

--- src/lib/create-model/add-get-method.js
+++ src/lib/create-model/add-get-method.js
@@ -20,49 +20,62 @@ module.exports = (context, config) => (model) => {
 
   if (GET !== false) {
     if (get(GET, 'custom', false) === false) {
-      model.get = (options) => {
-        context.logger.trace(options);
+      model.get = (options) =>
+        context.Promise.resolve().then(() => {
+          context.logger.trace(options);
 
-        const modelFields = get(GET, FIELDS, false);
-        const fields = get(options, FIELDS, modelFields);
-        const offset = get(options, OFFSET, false);
-        const modelPageSize = get(GET, PAGE_SIZE, DEFAULT_PAGE_SIZE);
-        const pageSize = get(options, PAGE_SIZE, modelPageSize);
-        const where = get(options, WHERE, false);
+          const modelFields = get(GET, FIELDS, false);
+          const fields = get(options, FIELDS, modelFields);
+          const offset = get(options, OFFSET, false);
+          const modelPageSize = get(GET, PAGE_SIZE, DEFAULT_PAGE_SIZE);
+          const pageSize = get(options, PAGE_SIZE, modelPageSize);
+          const where = get(options, WHERE, false);
 
-        let queryStringBuilder = [];
-        let pgFormatBuilder = [];
+          let queryStringBuilder = [];
+          let pgFormatBuilder = [];
 
-        queryStringBuilder.push('SELECT');
+          queryStringBuilder.push('SELECT');
 
-        if (fields === false) {
-          queryStringBuilder.push('*');
-        } else {
-          throw new Error('TODO: Implement fields implementation');
-        }
+          if (fields === false) {
+            queryStringBuilder.push('*');
+          } else {
+            if (!Array.isArray(fields)) {
+              throw new Error('fields must be an array of strings');
+            }
 
-        queryStringBuilder.push('FROM %I');
-        pgFormatBuilder.push(tableName);
+            fields.forEach((field, index) => {
+              if (index < fields.length - 1) {
+                queryStringBuilder.push('%I,');
+              } else {
+                queryStringBuilder.push('%I');
+              }
 
-        if (where !== false) {
-          throw new Error('TODO: Implement default where implementation');
-        }
-
-        if (pageSize !== false) {
-          queryStringBuilder.push('LIMIT %L');
-          pgFormatBuilder.push(pageSize);
-
-          if (offset !== false) {
-            queryStringBuilder.push('OFFSET %L');
-            pgFormatBuilder.push(offset);
+              pgFormatBuilder.push(field);
+            });
           }
-        }
 
-        return pool
-          .query(pgFormat(queryStringBuilder.join(' '), ...pgFormatBuilder))
-          .then(fp.get('rows'))
-          .then((data) => ({ response: { data } }));
-      };
+          queryStringBuilder.push('FROM %I');
+          pgFormatBuilder.push(tableName);
+
+          if (where !== false) {
+            throw new Error('TODO: Implement default where implementation');
+          }
+
+          if (pageSize !== false) {
+            queryStringBuilder.push('LIMIT %L');
+            pgFormatBuilder.push(pageSize);
+
+            if (offset !== false) {
+              queryStringBuilder.push('OFFSET %L');
+              pgFormatBuilder.push(offset);
+            }
+          }
+
+          return pool
+            .query(pgFormat(queryStringBuilder.join(' '), ...pgFormatBuilder))
+            .then(fp.get('rows'))
+            .then((data) => ({ response: { data } }));
+        });
     } else {
       throw new Error('TODO: Implement custom GET');
     }
--- test/unit/lib/create-model/add-get-method.js
+++ test/unit/lib/create-model/add-get-method.js
@@ -42,8 +42,9 @@ d(me, () => {
 
     set(pool, 'query', query);
 
-    set(context, POOL, pool);
     set(context, LOGGER, logger);
+    set(context, 'Promise', Promise);
+    set(context, POOL, pool);
     set(config, TABLE_NAME, tableName);
 
     set(config, GET_SEL, GET);
@@ -74,6 +75,73 @@ d(me, () => {
               `SELECT * FROM "${tableName}" LIMIT '10'`,
             ),
           ));
+    });
+
+    describe('given valid fields', () => {
+      let field1 = null;
+      let field2 = null;
+
+      beforeEach(() => {
+        field1 = `field-1-${uuid()}`;
+        field2 = `field-2-${uuid()}`;
+
+        fields = [field1, field2];
+
+        set(options, 'fields', fields);
+      });
+
+      it('should select by those fields and separate them by a comma', () =>
+        expect(addGetMethod(context, config)(model).get(options))
+          .to.eventually.be.fulfilled.then((resolution) =>
+            expect(resolution).to.have.nested.property(
+              'response.data',
+              queryResult,
+            ),
+          )
+          .then(() =>
+            expect(query).to.have.been.calledOnceWithExactly(
+              `SELECT "${field1}", "${field2}" FROM "${tableName}" LIMIT '10'`,
+            ),
+          ));
+    });
+
+    describe('given one valid field', () => {
+      let field1 = null;
+
+      beforeEach(() => {
+        field1 = `field-1-${uuid()}`;
+
+        fields = [field1];
+
+        set(options, 'fields', fields);
+      });
+
+      it('should select by that field', () =>
+        expect(addGetMethod(context, config)(model).get(options))
+          .to.eventually.be.fulfilled.then((resolution) =>
+            expect(resolution).to.have.nested.property(
+              'response.data',
+              queryResult,
+            ),
+          )
+          .then(() =>
+            expect(query).to.have.been.calledOnceWithExactly(
+              `SELECT "${field1}" FROM "${tableName}" LIMIT '10'`,
+            ),
+          ));
+    });
+
+    describe('given invalid fields', () => {
+      beforeEach(() => {
+        fields = null;
+
+        set(options, 'fields', fields);
+      });
+
+      it('should reject with an error', () =>
+        expect(
+          addGetMethod(context, config)(model).get(options),
+        ).to.eventually.be.rejectedWith('fields must be an array of strings'));
     });
   });
 });

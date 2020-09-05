e456e0185655e34013e7ac070b1707d63b18c867
Henry Brown <h@0ti.me>
2020-09-05 14:39:22 -0500

    Promisify the put and delete methods so they do not crash the request

--- src/lib/create-model/add-delete-method.js
+++ src/lib/create-model/add-delete-method.js
@@ -18,46 +18,47 @@ module.exports = (context, config) => (model) => {
 
   if (DELETE !== false) {
     if (get(DELETE, 'custom', false) === false) {
-      model.delete = (entity, options = {}) => {
-        context.logger.trace(options);
+      model.delete = (entity, options = {}) =>
+        Promise.resolve().then(() => {
+          context.logger.trace(options);
 
-        const allowDeleteMultiple = get(DELETE, ALLOW_DELETE_MULTIPLE, false);
-        const idField = get(DELETE, ID_FIELD, 'id');
-        const where = get(options, WHERE, false);
+          const allowDeleteMultiple = get(DELETE, ALLOW_DELETE_MULTIPLE, false);
+          const idField = get(DELETE, ID_FIELD, 'id');
+          const where = get(options, WHERE, false);
 
-        let pgFormatBuilder = [];
-        let queryStringBuilder = [];
-        let queryStringParameters = [];
+          let pgFormatBuilder = [];
+          let queryStringBuilder = [];
+          let queryStringParameters = [];
 
-        if (allowDeleteMultiple === true && where !== false) {
-          throw new Error(`implement deletion using where ${where}`);
-        } else {
-          const id = get(entity, idField, false);
+          if (allowDeleteMultiple === true && where !== false) {
+            throw new Error(`implement deletion using where ${where}`);
+          } else {
+            const id = get(entity, idField, false);
 
-          if (id === false) {
-            throw new Error(
-              `a single delete attempt failed because the ${idField} (${tableName}.${idField}) was not found`,
-            );
+            if (id === false) {
+              throw new Error(
+                `a single delete attempt failed because the ${idField} (${tableName}.${idField}) was not found`,
+              );
+            }
+
+            queryStringBuilder.push('DELETE FROM %I WHERE %I = $1;');
+            pgFormatBuilder.push(tableName);
+            pgFormatBuilder.push(idField);
+            queryStringParameters.push(id);
           }
 
-          queryStringBuilder.push('DELETE FROM %I WHERE %I = $1;');
-          pgFormatBuilder.push(tableName);
-          pgFormatBuilder.push(idField);
-          queryStringParameters.push(id);
-        }
+          const formattedQuery = pgFormat(
+            queryStringBuilder.join(' '),
+            ...pgFormatBuilder,
+          );
 
-        const formattedQuery = pgFormat(
-          queryStringBuilder.join(' '),
-          ...pgFormatBuilder,
-        );
-
-        return (queryStringParameters.length > 0
-          ? pool.query(formattedQuery, queryStringParameters)
-          : pool.query(formattedQuery)
-        )
-          .then(fp.get('rows'))
-          .then((data) => ({ response: { data } }));
-      };
+          return (queryStringParameters.length > 0
+            ? pool.query(formattedQuery, queryStringParameters)
+            : pool.query(formattedQuery)
+          )
+            .then(fp.get('rows'))
+            .then((data) => ({ response: { data } }));
+        });
     } else {
       throw new Error('TODO: Implement custom DELETE');
     }
--- src/lib/create-model/add-put-method.js
+++ src/lib/create-model/add-put-method.js
@@ -18,108 +18,109 @@ module.exports = (context, config) => (model) => {
 
   if (PUT !== false) {
     if (get(PUT, 'custom', false) === false) {
-      model.put = (entity, options = {}) => {
-        context.logger.trace(options);
+      model.put = (entity, options = {}) =>
+        Promise.resolve().then(() => {
+          context.logger.trace(options);
 
-        const assumeIdFieldMeansUpdate = get(
-          PUT,
-          ASSUME_ID_FIELD_MEANS_UPDATE,
-          true,
-        );
-        const fields = Object.keys(entity);
-        const idField = get(PUT, ID_FIELD, 'id');
-        const where = get(options, WHERE, false);
-
-        if (assumeIdFieldMeansUpdate !== true) {
-          // What should we do if this is the case? Are we going to allow inserting a new record with a hardcoded ID?
-          // That seems weird.
-          throw new Error(
-            `assume id field means update is not implemented tableName=${tableName}`,
+          const assumeIdFieldMeansUpdate = get(
+            PUT,
+            ASSUME_ID_FIELD_MEANS_UPDATE,
+            true,
           );
-        }
+          const fields = Object.keys(entity);
+          const idField = get(PUT, ID_FIELD, 'id');
+          const where = get(options, WHERE, false);
 
-        let pgFormatBuilder = [];
-        let queryStringBuilder = [];
-        let queryStringParameters = [];
-
-        if (get(entity, idField, false) === false) {
-          queryStringBuilder.push('INSERT INTO %I');
-          pgFormatBuilder.push(tableName);
-
-          if (Array.isArray(fields)) {
-            queryStringBuilder.push('(');
-
-            let addToEnd = [];
-
-            fields.forEach((field, i, ray) => {
-              queryStringParameters.push(get(entity, field));
-
-              if (i === ray.length - 1) {
-                queryStringBuilder.push('%I');
-                pgFormatBuilder.push(field);
-                addToEnd.push(`$${i + 1}`);
-              } else {
-                queryStringBuilder.push('%I ,');
-                pgFormatBuilder.push(field);
-                addToEnd.push(`$${i + 1} ,`);
-              }
-            });
-
-            queryStringBuilder.push(') VALUES (');
-
-            queryStringBuilder = queryStringBuilder.concat(addToEnd);
-
-            queryStringBuilder.push(');');
-          } else {
-            throw new Error('TODO: Implement fields implementation');
+          if (assumeIdFieldMeansUpdate !== true) {
+            // What should we do if this is the case? Are we going to allow inserting a new record with a hardcoded ID?
+            // That seems weird.
+            throw new Error(
+              `assume id field means update is not implemented tableName=${tableName}`,
+            );
           }
 
-          if (where !== false) {
-            throw new Error('TODO: Implement default where implementation');
-          }
-        } else {
-          queryStringBuilder.push('UPDATE %I SET');
-          pgFormatBuilder.push(tableName);
+          let pgFormatBuilder = [];
+          let queryStringBuilder = [];
+          let queryStringParameters = [];
 
-          let fixLast = false;
+          if (get(entity, idField, false) === false) {
+            queryStringBuilder.push('INSERT INTO %I');
+            pgFormatBuilder.push(tableName);
 
-          for (let i = 0; i < fields.length; ++i) {
-            if (fields[i] !== idField) {
-              queryStringBuilder.push(
-                `%I = $${queryStringParameters.length + 1},`,
-              );
-              pgFormatBuilder.push(fields[i]);
-              queryStringParameters.push(entity[fields[i]]);
+            if (Array.isArray(fields)) {
+              queryStringBuilder.push('(');
 
-              fixLast = true;
+              let addToEnd = [];
+
+              fields.forEach((field, i, ray) => {
+                queryStringParameters.push(get(entity, field));
+
+                if (i === ray.length - 1) {
+                  queryStringBuilder.push('%I');
+                  pgFormatBuilder.push(field);
+                  addToEnd.push(`$${i + 1}`);
+                } else {
+                  queryStringBuilder.push('%I ,');
+                  pgFormatBuilder.push(field);
+                  addToEnd.push(`$${i + 1} ,`);
+                }
+              });
+
+              queryStringBuilder.push(') VALUES (');
+
+              queryStringBuilder = queryStringBuilder.concat(addToEnd);
+
+              queryStringBuilder.push(');');
+            } else {
+              throw new Error('TODO: Implement fields implementation');
             }
+
+            if (where !== false) {
+              throw new Error('TODO: Implement default where implementation');
+            }
+          } else {
+            queryStringBuilder.push('UPDATE %I SET');
+            pgFormatBuilder.push(tableName);
+
+            let fixLast = false;
+
+            for (let i = 0; i < fields.length; ++i) {
+              if (fields[i] !== idField) {
+                queryStringBuilder.push(
+                  `%I = $${queryStringParameters.length + 1},`,
+                );
+                pgFormatBuilder.push(fields[i]);
+                queryStringParameters.push(entity[fields[i]]);
+
+                fixLast = true;
+              }
+            }
+
+            if (fixLast === true) {
+              const i = queryStringBuilder.length - 1;
+              queryStringBuilder[i] = queryStringBuilder[i].slice(0, -1);
+            }
+
+            // TODO: find every instance of shoving numbers into strings like this and use pgFormat instead.
+            queryStringBuilder.push(
+              `WHERE %I = $${queryStringParameters.length + 1};`,
+            );
+            pgFormatBuilder.push(idField);
+            queryStringParameters.push(entity[idField]);
           }
 
-          if (fixLast === true) {
-            const i = queryStringBuilder.length - 1;
-            queryStringBuilder[i] = queryStringBuilder[i].slice(0, -1);
-          }
-
-          // TODO: find every instance of shoving numbers into strings like this and use pgFormat instead.
-          queryStringBuilder.push(
-            `WHERE %I = $${queryStringParameters.length + 1};`,
+          const formattedQuery = pgFormat(
+            queryStringBuilder.join(' '),
+            ...pgFormatBuilder,
           );
-          pgFormatBuilder.push(idField);
-          queryStringParameters.push(entity[idField]);
-        }
 
-        const formattedQuery = pgFormat(
-          queryStringBuilder.join(' '),
-          ...pgFormatBuilder,
-        );
-
-        return (queryStringParameters.length > 0
-          ? pool.query(formattedQuery, queryStringParameters)
-          : pool.query(formattedQuery)
-        )
-          .then(fp.get('rows'))
-          .then((data) => ({ response: { data } }));
-      };
+          return (queryStringParameters.length > 0
+            ? pool.query(formattedQuery, queryStringParameters)
+            : pool.query(formattedQuery)
+          )
+            .then(fp.get('rows'))
+            .then((data) => ({ response: { data } }));
+        });
     } else {
       throw new Error('TODO: Implement custom PUT');
     }

bb03f82b7aa8d367c5b1de6fd4e40cd96944da31
Henry Brown <h@0ti.me>
2020-09-11 13:26:03 -0500

    Reverse the order of the scratch table creation and then reverse the array

--- db/scratch.js
+++ db/scratch.js
@@ -1,18 +1,36 @@
 /**
- * Order these in reverse version order, they get sorted later anyway.
+ * Order these in reverse version order.
  * Suggested rules:
  * - Database table names should be quoted, in upper camel case, and they should be singular nouns
  * - Database table field names should be lower camel case and they should be singular nouns
  * - Constraints such as foreign keys should use lower camel case
  */
 
-module.exports = [
+const reverse = (inp) => inp.reverse();
+
+module.exports = reverse([
   `
-    CREATE TABLE "Member" (
+    CREATE TABLE "Volunteer" (
       id BIGSERIAL NOT NULL,
-      name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      PRIMARY KEY(id)
+      userId BIGSERIAL NOT NULL,
+      PRIMARY KEY(id),
+      CONSTRAINT fkVolunteerToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
+    );
+  `,
+  `
+    CREATE TABLE "Staff" (
+      id BIGSERIAL NOT NULL,
+      userId BIGSERIAL NOT NULL,
+      PRIMARY KEY(id),
+      CONSTRAINT fkStaffToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
+    );
+  `,
+  `
+    CREATE TABLE "Donor" (
+      id BIGSERIAL NOT NULL,
+      userId BIGSERIAL NOT NULL,
+      PRIMARY KEY(id),
+      CONSTRAINT fkDonorToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
     );
   `,
   `
@@ -21,30 +39,14 @@ module.exports = [
       userId BIGSERIAL NOT NULL,
       PRIMARY KEY(id),
       CONSTRAINT fkClientToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
-    )
+    );
   `,
   `
-    CREATE TABLE "Donor" (
+    CREATE TABLE "Member" (
       id BIGSERIAL NOT NULL,
-      userId BIGSERIAL NOT NULL,
-      PRIMARY KEY(id),
-      CONSTRAINT fkDonorToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
-    )
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      PRIMARY KEY(id)
+    );
   `,
-  `
-    CREATE TABLE "Staff" (
-      id BIGSERIAL NOT NULL,
-      userId BIGSERIAL NOT NULL,
-      PRIMARY KEY(id),
-      CONSTRAINT fkStaffToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
-    )
-  `,
-  `
-    CREATE TABLE "Volunteer" (
-      id BIGSERIAL NOT NULL,
-      userId BIGSERIAL NOT NULL,
-      PRIMARY KEY(id),
-      CONSTRAINT fkVolunteerToMember FOREIGN KEY(userId) REFERENCES "Member"(id)
-    )
-  `,
-];
+]);

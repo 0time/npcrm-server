774b8b379408c7b221ec0839c5f1cf4f66ceec93
Henry Brown <h@0ti.me>
2020-09-15 16:36:31 -0500

    Fix -1 exit code to valid 127 exit code

--- src/server/express/stop.js
+++ src/server/express/stop.js
@@ -29,7 +29,7 @@ module.exports = (context) => {
       if (!resolved) {
         resolved = true;
 
-        context.process.exit(-1);
+        context.process.exit(127);
 
         reject(
           new Error(
